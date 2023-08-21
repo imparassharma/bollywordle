0fc63ffc6fc8a038d9855dae90d88b61b90b9e24
imparassharma <parasangiras121@gmail.com>
2023-08-21 13:13:55 +0530

    new

--- src/components/main-section.js
+++ src/components/main-section.js
@@ -17,12 +17,15 @@ function MainSection(){
     const [warning,updateWarn] = useState(false);
     const [isImage,handleImage] = useState(false);
     const game = document.getElementById("theGame");
+
     const won = document.getElementById("won");
     const over = document.getElementById("lost");
     const Value = inputValue.toUpperCase();
     const usedBar = document.getElementById("usedBar");
     const warn = document.getElementById("warn");
     const used = document.getElementById("used");
+    const top = document.getElementById("top");
+
 
     const handleBlur = (event) => {  //whenever any action taken after entering input value it will update/store that value
         setInputValue(event.target.value);
@@ -35,20 +38,22 @@ function MainSection(){
         console.log(Value);
         updateWarn(Value.includes("a") ||Value.includes("e") || Value.includes("i")||Value.includes("o")||Value.includes("u"))
     }
+
+    const handleBack = (event) =>{
+   
+    }
     
     const enterKeyPressed= (event) =>{
         if(event.key=="Enter") //function activated if pressed key is enter
         {
             console.log(warning);
             if(warning===true){
-                console.log("here");
                 return;
             }
             let updatedCorrect = correct;
             for(let i=0;i<arr.length;i++){
                 if(arr[i]===Value && !index.has(i) && Value!=["A","E","I","O","U"]){ //to check if correct guess is made again so no increment then
                     updatedCorrect++;  //if correct guess incrementing the correct value to keep check on correct guess
-                    console.log(updatedCorrect);
                     setindex((prevIndex) => new Set(prevIndex).add(i));  //pushing the index where value is matched
                 }
             }
@@ -62,19 +67,20 @@ function MainSection(){
                 setUsedStr(newUsedStr); //setting the new string value
                 const usedLettersElement = document.getElementById("usedBar");
                 usedLettersElement.innerHTML = newUsedStr;
+                setInputValue("");
             }
 
             if(life===1){ //when lives are 0 then game over message is generated
                 game.className = "theGame hidden";
                 over.className = "gameOver show";
                 used.classList.add("hidden");
+                top.classList.add("hidden");
             }
         }
     }
     useEffect(()=>{
         index.forEach((currentIndex) => {
             const element = document.getElementById(currentIndex);
-            console.log(Value);
             if(!element.classList.contains("green"))
             {
                 element.innerHTML = Value;
@@ -90,6 +96,7 @@ function MainSection(){
             game.className = "theGame hidden";
             won.className = "gameWon show";
             used.classList.add("hidden");
+            top.classList.add("hidden");
         }
     },[correct]);
 
@@ -104,14 +111,37 @@ function MainSection(){
         warningImage.onload = isImageLoaded; // Set the onLoad event handler
     }, []);
 
-    console.log(isImage);
+   /*useEffect(() => {
+        console.log(life);
+        const main = document.getElementById("main");
+        if (life <= 5) {
+          main.classList.add("low_life");
+        } else if (life ==3) {
+          main.classList.remove("low_life");
+          main.classList.add("verylow_life");
+        } else if(life==2){
+          main.classList.remove("veryLowLife");
+          main.classList.add("panic_life");
+        }
+      }, [life]);*/
+
+
+
     return(
         <div className="mainContainer">
             <div className="gameOver hidden" id="lost">
-                <h1>GAME OVER!!</h1>
+                <div className="result">
+                    <h1>GAME OVER!!</h1>
+                    <h2 id="ans">{arr}</h2>
+                </div>
+                <h3 className="back" id="backL" onClick={handleBack}>BACK</h3>
             </div>
             <div className="gameWon hidden" id="won">
-                <h1>YOU WON!!</h1>
+                <div className="result">
+                    <h1>GAME WON!!</h1>
+                    <h2 id="ans">{arr}</h2>
+                </div>
+                <h3 className="back" id="backW" onClick={handleBack}>BACK</h3>
             </div>
             <div className="theGame" id="theGame">
                 <div className="chances" id="chances">
--- src/components/top-section.js
+++ src/components/top-section.js
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import UsedBarComponent from "./usedBar";
 
 function TopSection(){
@@ -9,19 +9,23 @@ function TopSection(){
         const usedBar = document.getElementById("usedBar");
         const word = document.getElementById("word");
         const usedText = document.getElementById("usedText");
+        const DarkTheme = document.getElementById("DarkTheme");
+        const LightTheme = document.getElementById("LightTheme");
         usedText.classList.toggle("lightText");
-        usedBar.classList.toggle("light");
+        //usedBar.classList.toggle("light");
         main.classList.toggle("dark");
-        theme.classList.toggle("light");
         word.classList.toggle("lightText");
-        chances.classList.toggle("light");
-            
+        //chances.classList.toggle("light");
+        DarkTheme.classList.toggle("hidden");   
+        LightTheme.classList.toggle("hidden");
     }
+
   
     return(
-        <div className="topContainer">
-            <div className="bgcolor">
-                <div className="circle" onClick={handleClick} id="theme"></div>
+        <div className="topContainer" id="top">
+            <div className="toggle">
+                <div className="circle" onClick={handleClick} id="LightTheme"></div>
+                <div className="circle hidden"  onClick={handleClick} id="DarkTheme"></div>
             </div>
             <div className="history" id="used">
                 <h1 id="usedText">HISTORY</h1>

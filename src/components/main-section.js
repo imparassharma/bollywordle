8c7a5b1f8d4edbb111b85d3f29d71364beab5a1e
imparassharma <parasangiras121@gmail.com>
2023-08-22 12:46:56 +0530

    backtobasic

--- src/components/main-section.js
+++ src/components/main-section.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Block from "./CreateBlock";
 import {arr,vowelsCount} from "./Homepage";
+import UsedBarComponent from "./usedBar";
 import {Link} from "react-router-dom";
 import warningImg from "../images/warning.png";
 
@@ -14,14 +15,13 @@ function MainSection(){
     const [correct,setcorrect] = useState(vowelsCount);
     const [warning,updateWarn] = useState(false);
     const [isImage,handleImage] = useState(false);
-    const [clearFlag, setclearFlag] = useState(false);
-
     const game = document.getElementById("theGame");
-
 
     const won = document.getElementById("won");
     const over = document.getElementById("lost");
     const Value = inputValue.toUpperCase();
+    const usedBar = document.getElementById("usedBar");
+    const warn = document.getElementById("warn");
     const used = document.getElementById("used");
     const top = document.getElementById("top");
 
@@ -39,7 +39,7 @@ function MainSection(){
     }
     
     const enterKeyPressed= (event) =>{
-        if(event.key==="Enter") //function activated if pressed key is enter
+        if(event.key=="Enter") //function activated if pressed key is enter
         {
             console.log(warning);
             if(warning===true){
@@ -47,7 +47,7 @@ function MainSection(){
             }
             let updatedCorrect = correct;
             for(let i=0;i<arr.length;i++){
-                if(arr[i]===Value && !index.has(i) && Value!==["A","E","I","O","U"]){ //to check if correct guess is made again so no increment then
+                if(arr[i]===Value && !index.has(i) && Value!=["A","E","I","O","U"]){ //to check if correct guess is made again so no increment then
                     updatedCorrect++;  //if correct guess incrementing the correct value to keep check on correct guess
                     setindex((prevIndex) => new Set(prevIndex).add(i));  //pushing the index where value is matched
                 }
@@ -83,7 +83,7 @@ function MainSection(){
             }
             setInputValue("");
         })
-    },[index,Value]);
+    },[index]);
 
     //To keep check if all the correct guess are made
     useEffect(()=>{
@@ -121,15 +121,11 @@ function MainSection(){
       }, [life]);*/
 
       const handleClear =(event) =>{
-            setclearFlag(true);
+        const blockContainer = document.getElementById(blockContainer);
+        if (blockContainer) {
+            blockContainer.innerHTML = ""; // Clear the innerHTML
+        }
       }
-      
-      useEffect(() => {
-        console.log("herererere");
-        const blockContainer = document.getElementById("blockContainer");
-        blockContainer.innerHTML = "";
-    }, [clearFlag]);
-
 
 
     return(

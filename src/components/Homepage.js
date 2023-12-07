9722f307c56d494ac6762d05ec792c43ac3aa931
imparassharma <parasangiras121@gmail.com>
2023-12-07 13:32:25 +0530

    commit

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -133,7 +133,7 @@ function Homepage(){
 
     const singlePlayer =(event) =>{
         console.log("singlePlayer");
-        singleGame = "SINGHAM";
+        singleGame = "FUKREY";
         arr= Array.from(singleGame);
         const groupedItemList = [];
         let currentWord = []; // To store letters of the current word
